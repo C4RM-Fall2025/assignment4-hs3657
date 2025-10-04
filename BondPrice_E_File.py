ae96803235f15e27c366eda1e49c69d55c52663b
hs3657 <hs3657@columbia.edu>
2025-10-03 22:32:33 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -3,17 +3,16 @@
 def getBondPrice_E(face, couponRate, m, yc):
     coupon_payment = face * couponRate
     bond_price = 0
-
-    for t in range(1, m + 1): 
+    
+    for t, y in enumerate(yc, start=1):
         if t == m: 
             cash_flow = coupon_payment + face
         else:
             cash_flow = coupon_payment
-
-        pv_factor = 1 / (1 + yc[t - 1]) ** t
+        
+        pv_factor = 1 / (1 + y) ** t
         pvcf = cash_flow * pv_factor
-
-        bond_price += pvcf  
-
+        
+        bond_price += pvcf
+    
     return bond_price
-
